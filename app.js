a453e9b22045767a2bca18e4b1771bc762de07bb
cimlro <cimlro@be.ibm.com>
2019-09-25 13:33:59 +0200

    small change

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! Let's get finished with this tutorial ASAP");
+  res.send("Luc - Hello from Appsody! Let's get finished with this tutorial ASAP");
 });
  
 module.exports.app = app;
